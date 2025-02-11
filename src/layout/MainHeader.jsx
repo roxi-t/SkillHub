cdd9d7c2fdf125f0b5479cb032541b2d2db6c001
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2025-02-11 12:47:35 +0330

    feat: add overlay into the header

--- src/layout/MainHeader.jsx
+++ src/layout/MainHeader.jsx
@@ -23,32 +23,46 @@ const MainHeader = ({ isLogin }) => {
         {menuOpen ? <FiX /> : <FiMenu />}
       </button>
       <nav className="hidden md:flex gap-6 items-center font-yekan">
-        {links.map((link) => {
-          return (
-            <Link
-              key={link.route}
-              to={
-                link.isBlue ? (isLogin ? "/explorer" : link.route) : link.route
-              }
-              className={`text-[#494953] text-lg transition-colors duration-300 hover:text-[#6A7EFC] ${
-                link.isBlue
-                  ? "px-6 py-3 text-lg rounded-full border border-[#7161ef] text-[#494953] transition-all duration-300 hover:bg-[#6A7EFC] hover:text-white"
-                  : null
-              } ${
-                link.isRed
-                  ? "px-6 py-3 ml-3 text-lg rounded-full bg-[#FF5656] text-white transition-all duration-300 hover:bg-[#e13b3d] hover:text-white"
-                  : null
-              }`}
-            >
-              {link.text}
-            </Link>
-          );
-        })}
+        {links.map((link) => (
+          <Link
+            key={link.route}
+            to={link.isBlue ? (isLogin ? "/explorer" : link.route) : link.route}
+            className={`text-[#494953] text-lg transition-colors duration-300 hover:text-[#6A7EFC] ${
+              link.isBlue
+                ? "px-6 py-3 text-lg rounded-full border border-[#7161ef] text-[#494953] transition-all duration-300 hover:bg-[#6A7EFC] hover:text-white"
+                : ""
+            } ${
+              link.isRed
+                ? "px-6 py-3 ml-3 text-lg rounded-full bg-[#FF5656] text-white transition-all duration-300 hover:bg-[#e13b3d] hover:text-white"
+                : ""
+            }`}
+          >
+            {link.text}
+          </Link>
+        ))}
       </nav>
       {menuOpen && (
-        <div className="absolute top-16 left-0 w-full bg-[#EDF2F6] p-4 shadow-lg flex flex-col gap-4 text-center md:hidden z-10">
-          {links.map((link) => {
-            return (
+        <>
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 z-30 transition-opacity duration-300"
+            onClick={() => setMenuOpen(false)}
+          />
+          <div
+            className={`absolute top-0 left-0 w-full bg-[#EDF2F6] p-4 shadow-lg flex flex-col gap-4 text-center md:hidden z-40 transition-transform duration-300 ${
+              menuOpen
+                ? "translate-y-0 opacity-100"
+                : "-translate-y-10 opacity-0"
+            }`}
+          >
+            <button
+              className="md:hidden text-[#494953] text-2xl"
+              onClick={() => setMenuOpen(false)}
+              aria-label="Close menu"
+            >
+              <FiX />
+            </button>
+
+            {links.map((link) => (
               <Link
                 key={link.route}
                 to={
@@ -62,18 +76,18 @@ const MainHeader = ({ isLogin }) => {
                 className={`text-[#494953] text-lg transition-colors font-yekan duration-300 hover:text-[#6A7EFC] ${
                   link.isBlue
                     ? "w-full px-6 py-3 text-sm rounded-full border border-[#7161ef] text-[#494953] transition-all duration-300 hover:bg-[#6A7EFC] hover:text-white"
-                    : null
+                    : ""
                 } ${
                   link.isRed
-                    ? "w-full px-6 py-3 text-sm rounded-full bg-[#FF5656] text-white transition-all duration-300 hover:bg-[#e13b3d] hover:text-white"
-                    : null
+                    ? "w-full px-6 py-3 text-sm rounded-full mb-3 bg-[#FF5656] text-white transition-all duration-300 hover:bg-[#e13b3d] hover:text-white"
+                    : ""
                 }`}
               >
                 {link.text}
               </Link>
-            );
-          })}
-        </div>
+            ))}
+          </div>
+        </>
       )}
     </header>
   );
